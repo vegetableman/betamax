060fde5c9714324d11c2457395f8d2dd9ed08e1b
vignesh anand <vigneshanand17@gmail.com>
2023-11-11 12:11:58 +0530

    Updated encoder to support packing optimizations.

--- src/pyodide.worker.js
+++ src/pyodide.worker.js
@@ -1,6 +1,31 @@
 async function processImages(data) {
-  const {screenshots: images, times, format, resizeFactor} = data.payload;
+  const {screenshots: images, times, format, resizeFactor, packingMode} = data.payload;
   await pyodide.runPythonAsync(`
+    # Copyright (c) 2012, Sublime HQ Pty Ltd
+    # All rights reserved.
+
+    # Redistribution and use in source and binary forms, with or without
+    # modification, are permitted provided that the following conditions are met:
+    #     * Redistributions of source code must retain the above copyright
+    #       notice, this list of conditions and the following disclaimer.
+    #     * Redistributions in binary form must reproduce the above copyright
+    #       notice, this list of conditions and the following disclaimer in the
+    #       documentation and/or other materials provided with the distribution.
+    #     * Neither the name of the <organization> nor the
+    #       names of its contributors may be used to endorse or promote products
+    #       derived from this software without specific prior written permission.
+
+    # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+    # ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+    # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    # DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+    # DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+    # (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+    # LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+    # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+    # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    
     import io
     import base64
     import json
@@ -83,22 +108,70 @@ async function processImages(data) {
                         return row, col
         raise RuntimeError("Allocation error")
 
-    def find_matching_rect(bitmap, num_used_rows, packed, src, sx, sy, w, h):
-      template = src[sy:sy+h, sx:sx+w]
-      bh, bw = shape(bitmap)
-      image = packed[0:num_used_rows, 0:bw]
-  
+    def find_matching_rect(bitmap, num_used_rows, packed, src, sx, sy, w, h, packing_mode):
+      # Check if the number of used rows is less than the height of the template
       if num_used_rows < h:
           return None
   
-      result = cv2.matchTemplate(image,template,cv2.TM_CCOEFF_NORMED)
-  
-      row,col = unravel_index(result.argmax(),result.shape)
-      if ((packed[row:row+h,col:col+w] == src[sy:sy+h,sx:sx+w]).all()
-          and (packed[row:row+1,col:col+w,0] == src[sy:sy+1,sx:sx+w,0]).all()):
-          return row,col
+      # Extract the template and the relevant region from the packed image
+      template = src[sy:sy+h, sx:sx+w]
+      bh, bw = shape(bitmap)
+      image_region = packed[0:num_used_rows, 0:bw]
+
+      if packing_mode == 1.0:
+        result = cv2.matchTemplate(image_region, template, cv2.TM_CCOEFF_NORMED)
+
+        row,col = unravel_index(result.argmax(),result.shape)
+        if ((packed[row:row+h,col:col+w] == src[sy:sy+h,sx:sx+w]).all()
+            and (packed[row:row+1,col:col+w,0] == src[sy:sy+1,sx:sx+w,0]).all()):
+            return row,col
+        else:
+            return None
       else:
-          return None
+        # Check if the template or image region is empty
+        if template is None or template.shape[0] == 0 or template.shape[1] == 0:
+            print("Error: Template is empty.")
+            return None
+    
+        if image_region is None or image_region.shape[0] == 0 or image_region.shape[1] == 0:
+            print("Error: Image region is empty.")
+            return None
+    
+        # Use a single scale factor
+        scale = packing_mode
+
+        try:
+          # Calculate the destination size (dsize) for the resized images
+          dsize_template = (int(template.shape[1] * scale), int(template.shape[0] * scale))
+          dsize_image = (int(image_region.shape[1] * scale), int(image_region.shape[0] * scale))
+
+          if dsize_template[0] == 0 or dsize_template[1] == 0 or dsize_image[0] == 0 or dsize_image[1] == 0:
+            return None
+
+          # Resize the template and the image region
+          scaled_template = cv2.resize(template, dsize_template)
+          scaled_image = cv2.resize(image_region, dsize_image)
+      
+          # Perform template matching
+          result = cv2.matchTemplate(scaled_image, scaled_template, cv2.TM_CCOEFF_NORMED)
+      
+          # Find the location of the maximum correlation
+          row, col = unravel_index(result.argmax(), result.shape)
+      
+          # Rescale the coordinates to the original image size
+          row, col = int(row / scale), int(col / scale)
+      
+          # Check if the template matches the corresponding region in the packed image
+          if (
+              array_equal(packed[row:row+h, col:col+w], src[sy:sy+h, sx:sx+w])
+              and array_equal(packed[row:row+1, col:col+w, 0], src[sy:sy+1, sx:sx+w, 0])
+          ):
+              return row, col
+        except Exception as e:
+          # Handle any exceptions (e.g., division by zero, invalid resize)
+          print(f"Error: {e}")
+  
+        return None
 
     def slice_tuple_size(s):
       a, b = s
@@ -185,6 +258,7 @@ async function processImages(data) {
       print(f"Packing those differences...")
 
       rc = 1;
+      packing_mode = float('${packingMode}') if '${packingMode}' != 'null' else 1.0
       for size,i,j in rects_by_size:
         print(f"{rc}/{total_rects}")
         src = images[i]
@@ -194,15 +268,20 @@ async function processImages(data) {
         sx, sy = b.start, a.start
         w, h = b.stop - b.start, a.stop - a.start
 
-        existing = find_matching_rect(allocator.bitmap, allocator.num_used_rows, packed, src, sx, sy, w, h)
-        if existing:
-          dy, dx = existing
-          allocs[i][j] = (dy, dx)
+        if packing_mode > 0:
+          existing = find_matching_rect(allocator.bitmap, allocator.num_used_rows, packed, src, sx, sy, w, h, packing_mode)
+          if existing:
+            dy, dx = existing
+            allocs[i][j] = (dy, dx)
+          else:
+            dy, dx = allocator.allocate(w, h)
+            allocs[i][j] = (dy, dx)
+            packed[dy:dy+h, dx:dx+w] = src[sy:sy+h, sx:sx+w]
         else:
           dy, dx = allocator.allocate(w, h)
           allocs[i][j] = (dy, dx)
-
           packed[dy:dy+h, dx:dx+w] = src[sy:sy+h, sx:sx+w]
+
         rc = rc + 1
 
       packed = packed[0:allocator.num_used_rows]
